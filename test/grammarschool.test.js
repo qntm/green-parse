74fd9c9f602d7b2cbf5dee936e9265b2d1bf15b0
qntm <qntm@users.noreply.github.com>
2025-12-22 00:37:59 +0000

    Update grammarschool.test.js

--- test/grammarschool.test.js
+++ test/grammarschool.test.js
@@ -1,32 +1,32 @@
 /* eslint-env mocha */
 
-import assert from 'assert'
+import assert from 'node:assert/strict'
 
 import { fixed, or, seq, resolve } from '../src/matcher/index.js'
 
 describe('grammar school', () => {
   it('fixed', () => {
     const matchA = fixed('a')
-    assert.deepStrictEqual([...matchA.match('a', 0)], [{ j: 1, match: 'a' }])
+    assert.deepEqual([...matchA.match('a', 0)], [{ j: 1, match: 'a' }])
   })
 
   const matchLetter = or('abcdefghjklmnopqrstuvwxyz'.split(''))
 
   it('matchLetter', () => {
-    assert.deepStrictEqual([...matchLetter.match('f', 0)], [{ j: 1, match: 'f' }])
+    assert.deepEqual([...matchLetter.match('f', 0)], [{ j: 1, match: 'f' }])
   })
 
   const matchDigit = or('0123456789'.split(''))
 
   it('matchDigit', () => {
-    assert.deepStrictEqual([...matchDigit.match('1', 0)], [{ j: 1, match: '1' }])
+    assert.deepEqual([...matchDigit.match('1', 0)], [{ j: 1, match: '1' }])
   })
 
   const matchString = seq(['\'', matchDigit, '\''])
     .map(([open, string, close]) => ({ string }))
 
   it('matchString', () => {
-    assert.deepStrictEqual([...matchString.match('\'0\'', 0)], [{ j: 3, match: { string: '0' } }])
+    assert.deepEqual([...matchString.match('\'0\'', 0)], [{ j: 3, match: { string: '0' } }])
   })
 
   const matchNonTerminal = matchLetter
@@ -35,18 +35,18 @@ describe('grammar school', () => {
   const matchTerm = or([matchString, matchNonTerminal])
 
   it('matchTerm', () => {
-    assert.deepStrictEqual([...matchTerm.match('b', 0)], [{ j: 1, match: { nonTerminal: 'b' } }])
-    assert.deepStrictEqual([...matchTerm.match("'5'", 0)], [{ j: 3, match: { string: '5' } }])
+    assert.deepEqual([...matchTerm.match('b', 0)], [{ j: 1, match: { nonTerminal: 'b' } }])
+    assert.deepEqual([...matchTerm.match("'5'", 0)], [{ j: 3, match: { string: '5' } }])
   })
 
   const matchStarredTerm = seq([matchTerm, '*'])
     .map(([starredTerm, star]) => ({ starredTerm }))
 
   it('matchStarredTerm', () => {
-    assert.deepStrictEqual([...matchStarredTerm.match('b*', 0)], [
+    assert.deepEqual([...matchStarredTerm.match('b*', 0)], [
       { j: 2, match: { starredTerm: { nonTerminal: 'b' } } }
     ])
-    assert.deepStrictEqual([...matchStarredTerm.match("'5'*", 0)], [
+    assert.deepEqual([...matchStarredTerm.match("'5'*", 0)], [
       { j: 4, match: { starredTerm: { string: '5' } } }
     ])
   })
@@ -54,20 +54,20 @@ describe('grammar school', () => {
   const matchMaybeStarredTerm = or([matchStarredTerm, matchTerm])
 
   it('matchMaybeStarredTerm', () => {
-    assert.deepStrictEqual([...matchMaybeStarredTerm.match('b', 0)], [
+    assert.deepEqual([...matchMaybeStarredTerm.match('b', 0)], [
       { j: 1, match: { nonTerminal: 'b' } }
     ])
 
-    assert.deepStrictEqual([...matchMaybeStarredTerm.match('b*', 0)], [
+    assert.deepEqual([...matchMaybeStarredTerm.match('b*', 0)], [
       { j: 2, match: { starredTerm: { nonTerminal: 'b' } } },
       { j: 1, match: { nonTerminal: 'b' } }
     ])
 
-    assert.deepStrictEqual([...matchMaybeStarredTerm.match("'5'", 0)], [
+    assert.deepEqual([...matchMaybeStarredTerm.match("'5'", 0)], [
       { j: 3, match: { string: '5' } }
     ])
 
-    assert.deepStrictEqual([...matchMaybeStarredTerm.match("'5'*", 0)], [
+    assert.deepEqual([...matchMaybeStarredTerm.match("'5'*", 0)], [
       { j: 4, match: { starredTerm: { string: '5' } } },
       { j: 3, match: { string: '5' } }
     ])
@@ -78,7 +78,7 @@ describe('grammar school', () => {
   const matchProduction = matchSequence.plus('|')
 
   it('matchProduction', () => {
-    assert.deepStrictEqual([...matchProduction.match("a|a'5'*abc", 0)], [
+    assert.deepEqual([...matchProduction.match("a|a'5'*abc", 0)], [
       {
         j: 1,
         match: [[{ nonTerminal: 'a' }]]
@@ -110,7 +110,7 @@ describe('grammar school', () => {
     .map(([left, equals, production, right]) => [left, production])
 
   it('matchRule', () => {
-    assert.deepStrictEqual([...matchRule.match("a:='0'b'1'\n", 0)], [
+    assert.deepEqual([...matchRule.match("a:='0'b'1'\n", 0)], [
       {
         j: 11,
         match: [
@@ -125,7 +125,7 @@ describe('grammar school', () => {
     .map(rules => Object.fromEntries(rules))
 
   it('matchRules', () => {
-    assert.deepStrictEqual([...matchRules.match("a:='0'b'1'\nb:='2'|'3'*\n", 0)], [
+    assert.deepEqual([...matchRules.match("a:='0'b'1'\nb:='2'|'3'*\n", 0)], [
       {
         j: 11,
         match: {
@@ -174,14 +174,14 @@ describe('grammar school', () => {
   describe('rulesToMatchers', () => {
     it('one simple rule', () => {
       const matcher = [...rulesToMatchers.match("a:='0'\n", 0)][0].match.a
-      assert.deepStrictEqual([...matcher.match('0', 0)], [
+      assert.deepEqual([...matcher.match('0', 0)], [
         { j: 1, match: ['0'] }
       ])
     })
 
     it('one starred rule', () => {
       const matcher = [...rulesToMatchers.match("a:='3'*\n", 0)][0].match.a
-      assert.deepStrictEqual([...matcher.match('3', 0)], [
+      assert.deepEqual([...matcher.match('3', 0)], [
         { j: 0, match: [[]] },
         { j: 1, match: [['3']] }
       ])
@@ -189,28 +189,28 @@ describe('grammar school', () => {
 
     it('one rule', () => {
       const matcher = [...rulesToMatchers.match("a:='0''3'*'1'\n", 0)][0].match.a
-      assert.deepStrictEqual([...matcher.match('0331', 0)], [
+      assert.deepEqual([...matcher.match('0331', 0)], [
         { j: 4, match: ['0', ['3', '3'], '1'] }
       ])
     })
 
     it('two rules', () => {
       const matcher = [...rulesToMatchers.match("a:='0'b'1'\nb:='2'|'3'*\n", 0)][1].match.a
-      assert.deepStrictEqual([...matcher.match('0331', 0)], [
+      assert.deepEqual([...matcher.match('0331', 0)], [
         { j: 4, match: ['0', [['3', '3']], '1'] }
       ])
     })
 
     it('parses', () => {
       const matcher = [...rulesToMatchers.match("a:='0'b'1'\nb:='2'|'3'*\n", 0)][1].match.a
-      assert.deepStrictEqual([...matcher.parse('0331')], [
+      assert.deepEqual([...matcher.parse('0331')], [
         ['0', [['3', '3']], '1']
       ])
     })
 
     it('parse1s', () => {
       const matcher = [...rulesToMatchers.match("a:='0'b'1'\nb:='2'|'3'*\n", 0)][1].match.a
-      assert.deepStrictEqual(matcher.parse1('0331'), ['0', [['3', '3']], '1'])
+      assert.deepEqual(matcher.parse1('0331'), ['0', [['3', '3']], '1'])
     })
   })
 
@@ -225,11 +225,11 @@ describe('grammar school', () => {
       ])
     })).a
 
-    assert.deepStrictEqual([...matcher.match('01', 0)], [{ j: 2, match: ['0', [[]], '1'] }])
-    assert.deepStrictEqual([...matcher.match('021', 0)], [{ j: 3, match: ['0', ['2'], '1'] }])
-    assert.deepStrictEqual([...matcher.match('031', 0)], [{ j: 3, match: ['0', [['3']], '1'] }])
-    assert.deepStrictEqual([...matcher.match('0331', 0)], [{ j: 4, match: ['0', [['3', '3']], '1'] }])
-    assert.deepStrictEqual([...matcher.parse('0331')], [['0', [['3', '3']], '1']])
-    assert.deepStrictEqual(matcher.parse1('0331'), ['0', [['3', '3']], '1'])
+    assert.deepEqual([...matcher.match('01', 0)], [{ j: 2, match: ['0', [[]], '1'] }])
+    assert.deepEqual([...matcher.match('021', 0)], [{ j: 3, match: ['0', ['2'], '1'] }])
+    assert.deepEqual([...matcher.match('031', 0)], [{ j: 3, match: ['0', [['3']], '1'] }])
+    assert.deepEqual([...matcher.match('0331', 0)], [{ j: 4, match: ['0', [['3', '3']], '1'] }])
+    assert.deepEqual([...matcher.parse('0331')], [['0', [['3', '3']], '1']])
+    assert.deepEqual(matcher.parse1('0331'), ['0', [['3', '3']], '1'])
   })
 })
